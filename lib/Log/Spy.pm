b85d26ad5e731a9ef712528991b8ad8d65a23ac6
Rob N ★ <robn@robn.io>
2017-03-07 11:13:34 +1100

    Clarify multiple indexdirs

--- lib/Log/Spy.pm
+++ lib/Log/Spy.pm
@@ -12,7 +12,7 @@ sub usage_desc { "Usage: spy -d <index-dir> action [opts...] [args...]" }
 
 sub global_opt_spec {
   return (
-    [ "indexdir|d=s@" => "index location. Multiple locations can be specified by separating them with a colon. Most actions will only use the first one; see help on specific actions for more details.",
+    [ "indexdir|d=s@" => "index location. Multiples possible, but most actions will only use the first one; see help on specific actions for more details.",
       { required => 1 } ],
   );
 }
